f1ebf004c6687110c2e22e94baa0334fe8ba5aa0
AndyNtiamoah <126166717+AndyNtiamoah@users.noreply.github.com>
2023-03-30 20:46:54 +0200

    Add files via upload
    Added the logout button and all the features from chapter 10. some problemes with the flash messages when you login/register

--- capp/__init__.py
+++ capp/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import os
+from flask_login import LoginManager
 
 
 application = Flask(__name__)
@@ -15,8 +16,10 @@ application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 application.config['SQLALCHEMY_BINDS'] ={'transport': 'sqlite:///transport.db'}
 
 db = SQLAlchemy(application)
-
 bcrypt = Bcrypt(application)
+login_manager= LoginManager(application)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
 from capp.home.routes import home
 from capp.methodology.routes import methodology
