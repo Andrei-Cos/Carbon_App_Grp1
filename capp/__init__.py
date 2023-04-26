79c2e107af519541e7a35df229df6d70d6275e11
AndyNtiamoah <126166717+AndyNtiamoah@users.noreply.github.com>
2023-04-26 12:01:40 +0200

    Update __init__.py

--- capp/__init__.py
+++ capp/__init__.py
@@ -6,10 +6,9 @@ from flask_login import LoginManager
 
 
 application = Flask(__name__)
+  
 
-# application.config['SECRET_KEY'] = os.environ['SECRET_KEY']  
-
-application.config['SECRET_KEY'] = '23b3436c2bd9f7ee888955a2611f160471f78097432f6d3d'
+application.config['SECRET_KEY'] = os.environ['SECRET_KEY'] 
 
 # databases for users and transport 
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
